f27e099a8ce66e42b62b030cbe0b722f2f416190
jaydesai2304 <jaydesai2304@gmail.com>
2024-05-02 11:34:47 +0530

    modified

--- student/book/views.py
+++ student/book/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.template import loader 
-from .models import Member, Add, Contact, Wishlist
+from .models import Member, Add, Contact
 from django.contrib import messages
 
 def register(request):
@@ -193,8 +193,3 @@ def forgot(request):
                return redirect('login')
           messages.error(request,"invalid username")
      return render(request, 'forgot.html')
-
-def wishlist(request):
-
-
-     return render(request, 'wishlist.html')
